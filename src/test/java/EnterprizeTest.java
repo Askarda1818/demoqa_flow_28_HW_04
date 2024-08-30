f63bf57375500262ab5d641ce8f991effc9eb55d
Aslan.K.Kardanov <Aslan.K.kardanov@rt.ru>
2024-08-31 01:01:44 +0300

    added_08

---
+++ src/test/java/EnterprizeTest.java
@@ -0,0 +1,28 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.DragAndDropOptions.to;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class EnterprizeTest {
+
+    @Test
+    void EnterprizeOneTest(){
+        Configuration.browserSize = "1920x1080";
+        open("https://github.com");
+        $(byText("Solutions")).hover();
+        $(byText("Enterprise")).click();
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered developer platform."));
+    }
+    @Test
+    void DragAndDropTest(){
+        Configuration.browserSize = "1920x1080";
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDrop(to("#column-b"));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+
+}
